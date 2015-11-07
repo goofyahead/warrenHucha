ad62e0a8ee41f9d0a6220b62aca0cd10491b9c5c
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2015-11-07 02:31:52 +0100

    updated call

--- warrendisplay.py
+++ warrendisplay.py
@@ -14,12 +14,17 @@ GPIO.add_event_detect(21, GPIO.RISING)  # add rising edge detection on a channel
 
 while True:
 	time.sleep(0.1)
-	
+
+	# check amount of coins and value
+	r = requests.get("http://warren.ngrok.io/hucha")
+	print(r.json()["value"])
+
+	sense.show_message("Money in: " + r.json()["value"], text_colour=[0, 255, 0])
+
 	if GPIO.event_detected(21):
-		r = requests.get("http://warren.ngrok.io/coin")
-		print(r.json()["value"])
+		
 		sense.load_image("smile.png")
-    		print('Button pressed')
-		time.sleep(2)
+    	print('Button pressed')
+		time.sleep(1)
 		sense.load_image("fb.png")
 
