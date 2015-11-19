e205d3d38d675295325100b463144d902bcab66a
goofyahead <goofyahead@gmail.com>
2015-11-19 18:03:25 +0000

    changes

--- warrendisplay.py
+++ warrendisplay.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from sense_hat import SenseHat
 import RPi.GPIO as GPIO
 import time
@@ -8,32 +9,31 @@ GPIO.setup(21, GPIO.IN)
 
 sense = SenseHat()
 sense.set_rotation(180)
-sense.load_image("fb.png")
+#load initial image if wanted
 
-GPIO.add_event_detect(21, GPIO.RISING)  # add rising edge detection on a channel
+GPIO.add_event_detect(21, GPIO.FALLING, bouncetime=800)  # add rising edge detection on a channel
 currentCompany = "none"
+time_elapsed = 0
 
 while True:
 	time.sleep(0.1)
 
-	currentCompany = r.json()["company"]
-
 	if GPIO.event_detected(21):
-		update = requests.post("http://warren.ngrok.io/coin", data = {"key":"value"})
-		
 		sense.load_image("smile.png")
-	    	print('Button pressed')
+		response = requests.post("http://warren.ngrok.io/coin", data = {"key":"value"})
+		
+	    	time.sleep(1)
+		print('Button pressed')
+		sense.load_image(response.json()["selectedCompany"] + ".png")
+		time.sleep(1)
 
-	    sense.load_image(currentCompany + ".png")
-	    time.sleep(0.5)
-
-	    if (response.json()["coeficient"] > 1):
+		if (float(response.json()["coeficient"]) > 1):
 			sense.load_image("arrow_up.png")
+			time.sleep(1)
 		else:
 			sense.load_image("arrow_down.png")
-
-		sense.show_message("Coins: " + str(response.json()["value"] * response.json()["coeficient"]), text_colour=[0, 255, 0])
-
-		time.sleep(0.5)
+			time.sleep(1)
+		
+		sense.show_message(str( int(response.json()["value"]) * float(response.json()["coeficient"]) ) + "e", text_colour=[0, 255, 0])
 		
 
